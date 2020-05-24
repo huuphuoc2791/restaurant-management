b249bc3b99296cf80f54e08cfea3ef68fccf856c
Phuoc Le <phuoc.le@hdwebsoft.co>
2020-05-24 14:12:25 +0700

    Init project mobile app flutter

---
+++ lib/insta_home.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_insta_clone/insta_body.dart';
+
+class InstaHome extends StatelessWidget {
+  final topBar = new AppBar(
+    backgroundColor: new Color(0xfff8faf8),
+    brightness: Brightness.light,
+    centerTitle: true,
+    elevation: 1.0,
+    leading: new Icon(Icons.autorenew),
+    title: SizedBox(
+        height: 32.0, child: Text(
+      "Restaurant Management",
+      style: TextStyle(fontWeight: FontWeight.normal, fontFamily: "GreatVibes", fontSize: 24),
+    )),
+    actions: <Widget>[
+      Padding(
+        padding: const EdgeInsets.only(right: 12.0),
+        child: Icon(Icons.send),
+      )
+    ],
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: topBar,
+        body: new InstaBody(),
+        bottomNavigationBar: new Container(
+          color: Colors.white,
+          height: 60.0,
+          alignment: Alignment.center,
+          child: new BottomAppBar(
+            child: new Row(
+              // alignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                new IconButton(
+                  icon: Icon(
+                    Icons.home,
+                  ),
+                  onPressed: () {},
+                ),
+                new IconButton(
+                  icon: Icon(
+                    Icons.search,
+                  ),
+                  onPressed: null,
+                ),
+                new IconButton(
+                  icon: Icon(
+                    Icons.add_box,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  onPressed: null,
+                ),
+                new IconButton(
+                  icon: Icon(
+                    Icons.favorite,
+                    color: Colors.pinkAccent,
+                  ),
+                  onPressed: null,
+                ),
+                new IconButton(
+                  icon: Icon(
+                    Icons.info,
+                  ),
+                  onPressed: null,
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
