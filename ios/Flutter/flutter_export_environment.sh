281ac9b9b5016d4ceaebed4cc10182ff0764182f
Phuoc Le <phuoc.le@hdwebsoft.co>
2020-05-24 14:43:38 +0700

    Update info app

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/apple/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/apple/test/Flutter-Instagram-UI"
+export "FLUTTER_APPLICATION_PATH=/Users/apple/Projects/private/restaurant-management"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
